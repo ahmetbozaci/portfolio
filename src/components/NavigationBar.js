2a3b903f29a6059ca09d0cacb8b7757732b1dbd2
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-16 15:23:08 +0300

    Update navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,23 +1,19 @@
 import {
-  Nav, Navbar, NavDropdown, Container,
+  Nav, Navbar, Container,
 } from 'react-bootstrap';
 
 const NavigationBar = () => (
   <Navbar bg="light" expand="lg">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Ahmet Bozaci</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
+      <Navbar.Collapse id="basic-navbar-nav" className="text-right">
+        <Nav className="me-auto" />
+        <Nav>
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#link">Work</Nav.Link>
+          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#link">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
