bbd8d87da313b620b7aa0b8abdde4c5f1e93ec5e
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-18 02:21:15 +0300

    Update contact form action

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,24 +1,29 @@
 import { Form, Button } from 'react-bootstrap';
 
 const ContactForm = () => (
-  <Form className="w-50">
+  <Form
+    className="w-50"
+    action="https://formspree.io/f/xnqwzglq"
+    method="POST"
+  >
     <Form.Group className="mb-3" controlId="formBasicName">
       <Form.Label>Whats your name?</Form.Label>
-      <Form.Control type="text" placeholder="Name*" />
+      <Form.Control type="text" placeholder="Name*" name="name" />
     </Form.Group>
 
     <Form.Group className="mb-3" controlId="formBasicEmail">
       <Form.Label>Your email address</Form.Label>
-      <Form.Control type="email" placeholder="Email*" />
+      <Form.Control type="email" placeholder="Email*" name="email" />
     </Form.Group>
 
-    <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Group className="mb-3" controlId="formBasicText">
       <Form.Label>Type your message here</Form.Label>
       <Form.Control
         as="textarea"
         rows="5"
         maxLength="200"
         placeholder="Message*"
+        name="message"
       />
       <Form.Text className="text-muted">0/200</Form.Text>
     </Form.Group>
