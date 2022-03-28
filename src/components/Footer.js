93f3dfd27fe2408a7367a76bef97fb2c12920b90
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-28 18:16:22 +0300

    Update styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,17 @@ import { Nav } from 'react-bootstrap';
 import './Style/Footer.css';
 
 const Footer = () => (
-  <div className="text-center lead footer">
+  <div className="text-center lead footer text-light p">
     <Nav className="justify-content-center">
-      <Nav.Link href="mailto:ahmt9417@gmail.com"><FaMailBulk size={25} /></Nav.Link>
-      <Nav.Link href="https://github.com/ahmetbozaci"><FaGithub size={25} /></Nav.Link>
-      <Nav.Link href="https://www.linkedin.com/in/ahmetbozaci/"><FaLinkedin size={32} /></Nav.Link>
-      <Nav.Link href="https://twitter.com/ahmtbozaci"><FaTwitter size={25} /></Nav.Link>
+      <Nav.Link href="mailto:ahmt9417@gmail.com">
+        <FaMailBulk size={25} className="text-light" />
+      </Nav.Link>
+      <Nav.Link href="https://github.com/ahmetbozaci"><FaGithub size={25} className="text-light" /></Nav.Link>
+      <Nav.Link href="https://www.linkedin.com/in/ahmetbozaci/"><FaLinkedin size={32} className="text-light" /></Nav.Link>
+      <Nav.Link href="https://twitter.com/ahmtbozaci"><FaTwitter size={25} className="text-light" /></Nav.Link>
     </Nav>
-    <hr className="w-50 m-auto my-2 text-danger" />
-    <p className="text-muted">&copy; 2022 Ahmet Bozaci</p>
+    <hr className="w-50 m-auto my-2" />
+    <p className="my-2">&copy; 2022 Ahmet Bozaci</p>
   </div>
 );
 
