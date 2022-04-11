f37f0b29b6bb3d0ceb35029345c86a4f76720358
Ahmet Bozaci <ahmt9417@gmail.com>
2022-04-11 13:51:19 +0300

    Update font family

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,13 @@ const About = () => (
   <div className="d-flex justify-content-between about" id="about">
     <div className="align-self-center">
       <h3>About me</h3>
-      <p>Contact with me write something about yourself</p>
+      <p>dsa</p>
+      <p>
+        Look through some of my work and experience! If you like what you see and
+        <br />
+        have
+        project you need coded, don&apos;t hesisate to contact me.
+      </p>
       <a
         className="btn btn-primary"
         href="https://docs.google.com/document/d/e/2PACX-1vQa2q2dZIhcpw-obVNM-sKlyAhUjiBtPY27evjWxVusNbrBKugVYsHRcwItwOJql9jZv4sh3XgV9j73/pub"
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,27 @@
+/** @format */
+
 import './Style/Main.css';
 
 const Main = () => (
   <div className="main" id="main">
-    <h1 className="mb-4">Pretty code by ingenious person</h1>
+    <h1 className="mb-4">
+      Hello, I am
+      <br />
+      Ahmet Bozacı
+    </h1>
     <p>
       Hello I am a software developer!
       <br />
-      I can help you build a product, feature
-      or website.
-      Look through some of my work and experience!
+      I can help you build a product, feature or website. Look through some of
+      my work and experience!
       <br />
       <br />
-      If you like what
-      you see and have project you need code, dont hesitate to
+      If you like what you see and have project you need code, dont hesitate
+      to
       {' '}
-      <a href="#contact" className="text-decoration-none">contact me</a>
+      <a href="#contact" className="text-decoration-none">
+        contact me
+      </a>
     </p>
   </div>
 );
